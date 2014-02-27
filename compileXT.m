42a182c5e3c7a511962dd3e8b877c0c49a10847d
Bob Pepin <bobpepin@gmail.com>
2014-02-27 16:13:12 +0100

    Fix ImarisXT binary

--- compileXT.m
+++ compileXT.m
@@ -5,5 +5,7 @@
 % This generates yacudecu_proto.m and libyacudecu_thunk_pcwin64.dll
 loadlibrary('libyacudecu','yacudecu.h', 'mfilename', 'yacudecu_proto.m')
 
+copyfile('C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v5.5\bin\cufft64_55.dll', '.');
+
 % This uses the default paths to the CUFFT DLL and the Imaris JAR, adjust to match your system
-mcc -v -m XTYacuDeconv.m -a libyacudecu_thunk_pcwin64.dll -a 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v5.5\bin\cufft64_55.dll' -a libyacudecu.dll -a 'C:\Program Files\Bitplane\Imaris x64 7.6.5\XT\rtmatlab\ImarisLib.jar'
+mcc -v -m XTYacuDeconv.m -a libyacudecu_thunk_pcwin64.dll -a cufft64_55.dll -a libyacudecu.dll -a 'C:\Program Files\Bitplane\Imaris x64 7.6.5\XT\rtmatlab\ImarisLib.jar'
